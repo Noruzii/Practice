1354b170494224e9c7e0f40ee23447fb4ec1c879
Mohammad <mohammadnoruzii1383@gmail.com>
2025-07-14 10:36:54 +0330

    Update 1.cs

--- 1.cs
+++ 1.cs
@@ -5,7 +5,7 @@ public class 1 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        print("sdfsdf");
     }
 
     // Update is called once per frame
