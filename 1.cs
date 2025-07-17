3c342a0f714847b15ddd2147fd7f8e84d2a6efd4
MohammadNoruzi <mohammadnoruzii1383@gmail.com>
2025-07-17 17:35:28 +0330

    fs

--- 1.cs
+++ 1.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class 1 : MonoBehaviour
-{sdfasaffdsf
+{sdfasaffdsffsdfef
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
