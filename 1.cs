39c3a1b53e25064aebb6f19f1f3578be2f50e791
MohammadNoruzi <mohammadnoruzii1383@gmail.com>
2025-07-22 17:04:15 +0330

    Audio Bug Fixed

--- 1.cs
+++ 1.cs
@@ -5,7 +5,7 @@ public class 1 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+fdsafdsafgshdfsad
     void Update()
     {
         print("dfsafergdrfgdf")dfdsffdsafdsdsfdsf
